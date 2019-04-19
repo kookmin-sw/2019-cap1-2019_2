80d66f1a63d5085d70ee9de294bf1329396dc128
kimgayeon <yellow139@naver.com>
2019-04-19 17:28:31 +0900

    edit config.py

--- src/server/utils/config.py
+++ src/server/utils/config.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:src/mobile/utils/config.py
-WORKING_PATH = "/Users/csh/Documents/Github/2019-cap1-2019_2/src/mobile/"
-=======
 WORKING_PATH = "C:/Users/Ten/Documents/2019-cap1-2019_2/src/server"
->>>>>>> face_detection:src/server/utils/config.py
 
 TARGET_IMG = WORKING_PATH + '/images/sample/hj1.png'
 SOURCE_IMG = WORKING_PATH + '/images/sample/hj2.png'
