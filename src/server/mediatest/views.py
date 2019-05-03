fff2562463e9abcc93c07dbb10390a9ccb35246b
dhlee <dhlee130@naver.com>
2019-05-03 16:27:25 +0900

    text파일 서버전송 및 response 이미지저장

--- src/server/mediatest/views.py
+++ src/server/mediatest/views.py
@@ -11,17 +11,35 @@ from django.http import HttpResponse
 @csrf_exempt
 def upload(request):
     if request.method == 'POST':
-        myfile = request.FILES['file']
-        print(myfile.name)
-        path = default_storage.save(myfile.name, ContentFile(myfile.read()))
+        imgfile = request.FILES['data_img']
+        path = default_storage.save(imgfile.name, ContentFile(imgfile.read()))
+        print(imgfile.name + " saved OK")
         
-        
-        #fs = FileSystemStorage()
-        
-        #filename = fs.save(myfile.name, myfile, encoding="UTF8")
-        #uploaded_file_url = fs.url(filename)
+        txtfile1 = request.FILES['data_tt']
+        path = default_storage.save(txtfile1.name, ContentFile(txtfile1.read()))
+        print(txtfile1.name + " saved OK")
 
+        txtfile2 = request.FILES['data_tu']
+        path = default_storage.save(txtfile2.name, ContentFile(txtfile2.read()))
+        print(txtfile2.name + " saved OK")
+
+        txtfile3 = request.FILES['data_tv']
+        path = default_storage.save(txtfile3.name, ContentFile(txtfile3.read()))
+        print(txtfile3.name + " saved OK")
+        
+        txtfile4 = request.FILES['data_mt']
+        path = default_storage.save(txtfile4.name, ContentFile(txtfile4.read()))
+        print(txtfile4.name + " saved OK")
+
+        txtfile5 = request.FILES['data_mu']
+        path = default_storage.save(txtfile5.name, ContentFile(txtfile5.read()))
+        print(txtfile5.name + " saved OK")
+
+        txtfile6 = request.FILES['data_mv']
+        path = default_storage.save(txtfile6.name, ContentFile(txtfile6.read()))
+        print(txtfile6.name + " saved OK")
     
 
-    ret = {'success': "true", 'message' : "server-hello"}
-    return HttpResponse(json.dumps(ret), "application/json")
+    base_img = open("./pbk1.png", "rb").read()
+    
+    return HttpResponse(base_img, content_type="image/png")
