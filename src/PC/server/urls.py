fd905338c68749be40f6c3f48168a4bc463c6811
sangYeol <ten_ability@daum.net>
2019-05-08 20:24:55 +0900

    디렉토리 변경 및 서버 최적화

--- src/PC/server/urls.py
+++ src/PC/server/urls.py
@@ -1,4 +1,4 @@
-"""tserver URL Configuration
+"""server URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include,url
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^mediatest/', include('mediatest.urls')),
+    url(r'happiness', views.happiness, name='happiness')
 ]
