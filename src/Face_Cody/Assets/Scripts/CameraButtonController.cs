496f00a6cfb2feda0bce4705726feb5a81c44d64
kimgayeon <yellow139@naver.com>
2019-05-10 14:48:00 +0900

    edit bugs

--- src/Face_Cody/Assets/Scripts/CameraButtonController.cs
+++ src/Face_Cody/Assets/Scripts/CameraButtonController.cs
@@ -1,18 +1,22 @@
 ﻿using UnityEngine;
+//using System.Collections;
 
 public class CameraButtonController : MonoBehaviour
 {
+    //private AudioSource cameraAudio;
     private GameObject faceOccluder;
     private GameObject screenCapturer;
 
     void Start()
     {
+        //cameraAudio = GetComponent<AudioSource>();
         faceOccluder = GameObject.Find("FaceOccluder");
         screenCapturer = GameObject.Find("ScreenCapturer");
     }
 
     public void onClick()
     {
+        //cameraAudio.Play();
         string name = System.DateTime.Now.ToString("yyMMdd_HHmmss");
         screenCapturer.GetComponent<ScreenCaptureController>().ScreenCapture(name);
         faceOccluder.GetComponent<GoogleARCore.Examples.AugmentedFaces.ARCoreAugmentedFaceMeshFilter>().SaveMeshInfo(name);
